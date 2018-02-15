8f92f8c90365c8042a6f9738a8319aeb80b94204
James Gibson <iam@jamesgibson.co>
2018-02-14 18:05:42 -0700

    Removed console.log

--- src/services/data/redux/rootReducer.mjs
+++ src/services/data/redux/rootReducer.mjs
@@ -5,8 +5,6 @@ import applicationControl from './reducers/applicationControl.mjs';
 import deviceControl from './reducers/deviceControl.mjs';
 import researchControl from './reducers/researchControl.mjs';
 
-console.log(Redux)
-
 const rootReducer = Redux.combineReducers({
     applicationControl,
     researchControl,
